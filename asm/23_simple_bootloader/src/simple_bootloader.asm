b821a6a39b21eda7f75bc1b37bc3bbb78243ce8a
jotavare <jpoliveira.mm@gmail.com>
2024-05-03 19:02:31 +0100

    create a simple bootloader asm file

---
+++ asm/23_simple_bootloader/src/simple_bootloader.asm
@@ -0,0 +1,12 @@
+org 0x7c00              ; set origin to 0x7c00, default address where the BIOS loads the bootloader
+
+bits 16                 ; set the assembly to 16-bit mode, typical for bootloader code
+
+main:
+    hlt                 ; Halt the processor, essentially doing nothing
+
+halt:
+    jmp halt            ; Infinite loop, jumping back to itself, effectively halting the processor indefinitely
+
+times 510-($-$$) db 0   ; fill the rest of the bootloader space with zeros to ensure it's padded up to 510 bytes
+dw 0aa55h               ; boot signature, indicating to the BIOS that this is a bootable device
